57628149e4e76fd5129cb69bf2e81effc47dba61
SCI-HI admin <chiang@ukzn.ac.za>
2018-04-18 12:49:01 +0200

    prizm_housekeeping.py edited to add mosfet, GPIO cleanup ~ LP

--- prizm_housekeeping.py
+++ prizm_housekeeping.py
@@ -43,21 +43,28 @@ def run_switch(opts, start_time=None):
         srcs = {'antenna' : opts.antenna,
                 'res100' : opts.res100,
                 'res50' : opts.res50,
-                'short' : opts.short}
-#                'noise' : opts.noise}
+                'short' : opts.short,
+                'noise' : opts.noise,
+                'opn' : opts.open}
 
         # Set GPIO mode for all selected sources
         GPIO.setmode(GPIO.BCM)
+        GPIO.setup(opts.reset, GPIO.OUT)   
+        GPIO.output(opts.reset, GPIO.LOW)
+        GPIO.setup(opts.mosfet, GPIO.OUT)
+        GPIO.output(opts.mosfet, GPIO.LOW)
         for src in opts.seq:
                 pin = srcs[src][0]
                 GPIO.setup(pin, GPIO.OUT)  # Define the pins as outputs
                 GPIO.output(pin, GPIO.LOW) # All pins set to LOW i.e. 0V
                 print 'GPIO pin', pin, 'set to output. Initial state:LOW'
         # Open log file
-        if start_time is None:
+        try:
+            
+            if start_time is None:
                 start_time = datetime.datetime.utcnow().strftime('%Y%m%d_%H%M%S')
         
-        while True:
+            while True:
                 tstart = time.time()
                 starttime = datetime.datetime.utcnow().strftime('%Y%m%d_%H%M%S')
                 if (tstart>1e5):
@@ -73,32 +80,41 @@ def run_switch(opts, start_time=None):
                 res50 =  scio.scio(outsubdir+'/res50.scio',compress=compress)
                 res100 =  scio.scio(outsubdir+'/res100.scio',compress=compress)
                 short =  scio.scio(outsubdir+'/short.scio',compress=compress)
-                        
+                noise =  scio.scio(outsubdir+'/noise.scio',compress=compress)
+                open =  scio.scio(outsubdir+'/open.scio',compress=compress)
                 while time.time()-tstart < opts.tfile*60:
                         for src in opts.seq:
                                 if src == 'antenna': arr=antenna
                                 if src == 'res50': arr=res50
                                 if src == 'res100': arr=res100
                                 if src == 'short': arr=short
+                                if src == 'noise' : arr=noise
+				                if src == 'open' : arr=open
                                 
                                 pin = srcs[src][0]
                                 ontime = float(srcs[src][1]) * 60.   # Convert to seconds
                                 starttime = datetime.datetime.utcnow().strftime('%Y%m%d_%H%M%S')
-                                GPIO.output(pin,1)
-                                t_start=time.time()
-                                arr.append(nm.array([1,t_start]))
-                                starttxt = starttime + ' : ' + src + ' on'
-                                print starttxt
-                                time.sleep(ontime)
+                                print 'Reset On'
+                                GPIO.output(opts.reset,1)
+                                time.sleep(0.20)
+                                GPIO.output(opts.reset,0);time.sleep(0.20);print 'Reset off'
+                                if src == 'noise': GPIO.output(21,1); print 'mosfet on'
+                                GPIO.output(pin,1);print '%s Src On'%src
+                                time.sleep(0.20)
+                                print '%s Src Off'%src
                                 GPIO.output(pin,0)
+                               # if src == 'noise': GPIO.output(21,0); print 'mosfet off'
+                                t_start=time.time()
+                                arr.append(nm.array([1,t_start])) 
+                                time.sleep(ontime)
+                                if src == 'noise': GPIO.output(21,0); print 'mosfet off'
                                 t_stop=time.time()
                                 arr.append(nm.array([0,t_stop]))
-                                stoptime = datetime.datetime.utcnow().strftime('%Y%m%d_%H%M%S')
-                                stoptxt = stoptime + ' : ' + src + ' off'
-                                print stoptxt
                                 
-                                
-#        GPIO.cleanup() 
+        except KeyboardInterrupt:
+            print 'Keyboard Interrupt : Cleaning all used GPIOs'
+            GPIO.cleanup()
+            print 'All used GPIOs cleaned'
         return
 
 #=======================================================================
@@ -215,19 +231,23 @@ if __name__ == '__main__':
     parser.set_usage('switch_control.py [options]')
     parser.set_description(__doc__)
     #parser.add_option('-l', '--logdir', dest='logdir',type='str', default='/data/housekeeping',help='Log directory [default: %default]')
-    parser.add_option('-o', '--order', dest='seq',type='string', default=['antenna','res100','res50','short','noise'],
-        	      help='Desired switch sequence of sources, options are "antenna", "res100", "res50", "short", "noise" [default: %default]',
+    parser.add_option('-o', '--order', dest='seq',type='string', default=['antenna','res100','res50','short','noise','opn'],
+        	      help='Desired switch sequence of sources, options are "antenna", "res100", "res50", "short", "noise", "open" [default: %default]',
                       action='callback', callback=seq_callback)
-    parser.add_option('-a', '--antenna', dest='antenna', type='string',default=[1,60],
+    parser.add_option('--reset', dest='reset', type='int', help='Reset GPIO pin # : 18')
+    parser.add_option('-a', '--antenna', dest='antenna', type='string',default=[23,10],
 		      help='Antenna: GPIO pin # and # of minutes [default: %default]', action='callback', callback=src_callback)
-    parser.add_option('-R', '--res100', dest='res100', type='string',default=[2,1],
+    parser.add_option('-R', '--res100', dest='res100', type='string',default=[7,1],
 		      help='100 ohm resistor: GPIO pin # and # of minutes [default: %default]', action='callback', callback=src_callback)
-    parser.add_option('-r', '--res50', dest='res50', type='string',default=[3,1],
+    parser.add_option('-r', '--res50', dest='res50', type='string',default=[24,1],
 		      help='50 ohm resistor: GPIO pin # and # of minutes [default: %default]', action='callback', callback=src_callback)
-    parser.add_option('-s', '--short', dest='short', type='string',default=[4,1],
+    parser.add_option('-s', '--short', dest='short', type='string',default=[25,1],
 		      help='Short: GPIO pin # and # of minutes [default: %default]', action='callback', callback=src_callback)
-    parser.add_option('-n', '--noise', dest='noise', type='string',default=[5,1],
+    parser.add_option('-n', '--noise', dest='noise', type='string',default=[8,1],
 		      help='Noise: GPIO pin # and # of minutes [default: %default]', action='callback', callback=src_callback)
+    parser.add_option('-O', '--open', dest='open', type='string',default=[12,1],
+		      help='Open: GPIO pin # and # of minutes [default: %default]', action='callback', callback=src_callback)
+    parser.add_option('-m', '--mosfet', dest='mosfet', type='int', help='Mosfet GPIO pin #: 21')
     parser.add_option('-p', '--ptemp', dest='ptemp',type='str', default='/sys/class/thermal/thermal_zone0/temp',
 		      help='Pi temperature read location [default: %default]')
     parser.add_option('-d', '--device', dest='tdev',type='str', default='/sys/bus/w1/devices/w1_bus_master1/*/w1_slave',
