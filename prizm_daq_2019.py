a9015c76db39cf7883cf24c70725635e36d39c4a
nivekg <nivekghazi94@gmail.com>
2020-03-17 13:43:56 +0200

    Consolidated code from prizm_housekeeping.py and prizm_daq_2019.py into a single script

--- prizm_daq_2019.py
+++ prizm_daq_2019.py
@@ -457,6 +457,219 @@ def acquire_data_singlesnap(fpga,opts,npol=4,ndat=2048,wait_for_new=True):
 		#return
 
 #=======================================================================
+def run_switch(opts, start_time=None):
+        """Run the switch.  Cycle between sources by turning on appropriate
+        Raspberry Pi GPIO pins for user-specified lengths of time.
+        - opts : options from parser
+        - start_time : optional starting time stamp for the log file
+        """
+
+        # Define dictionary of sources to switch between.  For each
+        # source, specify a two-element list consisting of GPIO pin number
+        # and number of MINUTES for source to remain selected.
+        srcs = {'antenna' : opts.antenna,
+                'res100' : opts.res100,
+                'res50' : opts.res50,
+                'short' : opts.short,
+                'noise' : opts.noise,
+                'open' : opts.open}
+
+        # Set GPIO mode for all selected sources
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(opts.reset, GPIO.OUT)   
+        GPIO.output(opts.reset, GPIO.LOW)
+        GPIO.setup(opts.mosfet, GPIO.OUT)
+        GPIO.output(opts.mosfet, GPIO.LOW)
+        for src in opts.seq:
+                pin = srcs[src][0]
+                GPIO.setup(pin, GPIO.OUT)  # Define the pins as outputs
+                GPIO.output(pin, GPIO.LOW) # All pins set to LOW i.e. 0V
+                print 'GPIO pin', pin, 'set to output. Initial state:LOW'
+        # Open log file
+        if start_time is None:
+                start_time = datetime.datetime.utcnow().strftime('%Y%m%d_%H%M%S')
+        
+        while True:
+                tstart = time.time()
+                starttime = datetime.datetime.utcnow().strftime('%Y%m%d_%H%M%S')
+                if (tstart>1e5):
+                        tfrag=repr(tstart)[:5]
+                else:
+                        print 'warning in run_switch - tstart seems to be near zero.  Did you set your clock?'
+                        tfrag='00000'
+                outsubdir = opts.outdir + '/' + tfrag + '/' + str(nm.int64(tstart))
+                if not os.path.isdir(outsubdir):
+                        os.makedirs(outsubdir)
+                
+                compress=opts.compress
+                antenna =  scio.scio(outsubdir+'/antenna.scio',compress=compress)
+                res50 =  scio.scio(outsubdir+'/res50.scio',compress=compress)
+                res100 =  scio.scio(outsubdir+'/res100.scio',compress=compress)
+                short =  scio.scio(outsubdir+'/short.scio',compress=compress)
+                noise =  scio.scio(outsubdir+'/noise.scio',compress=compress)
+                open =  scio.scio(outsubdir+'/open.scio',compress=compress)
+                while time.time()-tstart < opts.tfile*60:
+                        for src in opts.seq:
+                                if src == 'antenna': arr=antenna
+                                if src == 'res50': arr=res50
+                                if src == 'res100': arr=res100
+                                if src == 'short': arr=short
+                                if src == 'noise' : arr=noise
+				if src == 'open' : arr=open
+                                
+                                pin = srcs[src][0]
+                                ontime = float(srcs[src][1]) * 60.   # Convert to seconds
+                                starttime = datetime.datetime.utcnow().strftime('%Y%m%d_%H%M%S')
+                                print 'Reset On'
+                                GPIO.output(opts.reset,1)
+                                time.sleep(0.20)
+                                GPIO.output(opts.reset,0);time.sleep(0.20);print 'Reset off'
+                                # Special case for noise source: need to turn mosfet on as well
+                                if src == 'noise': GPIO.output(opts.mosfet,1); print 'mosfet on'
+                                # Set switch to the appropriate
+                                # source.  These are latching
+                                # switches, so just need to pulse the
+                                # pin...
+                                GPIO.output(pin,1);print '%s Src On'%src
+                                time.sleep(0.20)
+                                # ...and then immediately turn off again
+                                print '%s Src Off'%src
+                                GPIO.output(pin,0)
+                                # Record time stamps for how long
+                                # we're on this switch position.  Note
+                                # that we're using system time here,
+                                # will need to correct with RTC time
+                                # in post-processing.
+                                t_start=time.time()
+                                arr.append(nm.array([1,t_start])) 
+                                time.sleep(ontime)
+                                if src == 'noise': GPIO.output(opts.mosfet,0); print 'mosfet off'
+                                t_stop=time.time()
+                                arr.append(nm.array([0,t_stop]))
+                                sys.stdout.flush()
+        return
+
+#=======================================================================
+def read_temperatures(opts, start_time=None):
+
+        """Read temperatures and log to file at specified time intervals
+        - opts : options from parser, including time interval
+        - start_time : optional starting time stamp for the log file
+        """
+
+        # Open a log file and write some header information
+        if start_time is None:
+                start_time = datetime.datetime.utcnow().strftime('%Y%m%d_%H%M%S')
+
+        # Get temperature sensor info from config file
+        f = open(opts.tconf, 'r')
+        txt = f.readlines()
+        tempsensors = []
+        for line in txt:
+                # Check if leading character is a comment tag
+                s = re.search(r'^\s*\#', line)
+                if s is not None:
+                        continue
+                # Otherwise assume that there are three fields: sensor
+                # ID, tag, long descriptor; skip everything else
+                fields = line.split(',')
+                if len(fields) != 3:
+                        continue
+                id = fields[0].strip()
+                tag = fields[1].strip()
+                desc = fields[2].strip()  # unused for now, useful for print debugging
+                tempsensors.append((id, tag))  # use list instead of dict to preserve order
+        f.close()
+                
+        # Do an infinite loop over temperature reads
+        while True:
+                tstart = time.time()
+                #starttime = datetime.datetime.utcnow()#.strftime('%Y%m%d_%H%M%S')
+                if (tstart>1e5):
+                        tfrag=repr(tstart)[:5]
+                else:
+                        print 'warning in run_switch - tstart seems to be near zero.\
+  Did you set your clock?'
+                        tfrag='00000'
+                outsubdir = opts.outdir+'/'+tfrag +'/' + str(nm.int64(tstart))
+                if not os.path.isdir(outsubdir):
+                        os.makedirs(outsubdir)
+                # One wire sensors take some time to read, so record
+                # both start and stop times for each logging cycle.
+                # Do this for both system time and attempted RTC
+                # reads.
+		f_pi_time_sys = open(outsubdir + '/time_sys_pi.raw','w')
+		f_pi_time_rtc = open(outsubdir + '/time_rtc_pi.raw','w')
+        	f_therms_time_start = open(outsubdir + '/time_start_therms.raw','w')
+        	f_therms_time_stop = open(outsubdir + '/time_stop_therms.raw','w')
+                # File for Pi temperature
+                f_pi_temp =  open(outsubdir+'/temp_pi.raw','w')
+                # Files for one-wire sensors: specified in config file
+                f_therms_temp = []
+                for tempsensor in tempsensors:
+                        tag = tempsensor[1]
+                        f = open(outsubdir + '/temp_'+tag+'.raw','w')
+                        f_therms_temp.append(f)
+
+                # Start reading sensors
+                while time.time()-tstart < opts.tfile*60:
+
+                        # Read Pi time (system and RTC) and temperature
+                        time_start_sys = time.time()
+                        time_start_rtc = read_rtc_datetime()
+                        #tstamp = datetime.datetime.utcnow().strftime('%Y%m%d_%H%M%S')
+			nm.array(time_start_sys).tofile(f_pi_time_sys)
+			nm.array(time_start_rtc).tofile(f_pi_time_rtc)
+			pi_temperature = subprocess.check_output('cat '+opts.ptemp,shell=True)
+                        my_tmp = nm.int32(pi_temperature)
+			nm.array(my_tmp).tofile(f_pi_temp)
+
+                        # Read one-wire sensors
+			tstamp = datetime.datetime.utcnow().strftime('%Y%m%d_%H%M%S')
+			print 'Starting one-wire read : ', tstamp
+			time_start = time.time()
+			nm.array(time_start).tofile(f_therms_time_start)
+                        for i,tempsensor in enumerate(tempsensors):
+                                id = tempsensor[0]
+                                tag = tempsensor[1]
+                                # Replace device wildcard with actual sensor ID and do a read
+				try:
+                                	dfile = open(opts.tdev.replace('*', id))
+                                	txt = dfile.read()
+                                	dfile.close()
+				except:
+					txt = None
+                                # Search for e.g. "t=25345" string for temperature reading
+				temperature = nm.NAN
+				if txt is not None:
+                                	s = re.search(r't=(\d+)', txt)
+                                	if s is not None:
+                                        	temperature = float(s.group(1)) / 1000
+                                        	nm.array(temperature).tofile(f_therms_temp[i])
+                                print tag, '\t', temperature
+			time_stop = time.time()
+			nm.array(time_stop).tofile(f_therms_time_stop)
+
+			f_pi_temp.flush()
+			f_pi_time_sys.flush()
+			f_pi_time_rtc.flush()
+			f_therms_time_start.flush()
+			f_therms_time_stop.flush()
+                        for f in f_therms_temp:
+                                f.flush()
+			sys.stdout.flush()
+                        time.sleep(opts.temptime*60)
+
+                # Hmm, we weren't closing the files properly last year?
+                f_pi_temp.close()
+                f_pi_time_sys.close()
+                f_pi_time_rtc.close()
+                f_therms_time_start.close()
+                f_therms_time_stop.close()
+                for f in f_therms_temp:
+                        f.close()
+        return
+
 if __name__ == '__main__':
 
         # Parse options
@@ -543,6 +756,12 @@ if __name__ == '__main__':
         logging.info('Writing data to top level location %s' %(opts.outdir))
 	try:
                 if daq is 'prizm':
+                        # Start up switch operations and temperature logging, use same starting time stamp for both
+                        start_time = datetime.datetime.utcnow().strftime('%Y%m%d_%H%M%S')
+                        p1 = thread.start_new_thread( run_switch, (opts, start_time) )
+                        # Tiny sleep statement to avoid directory creation collision
+                        time.sleep(2)
+                        p2 = thread.start_new_thread( read_temperatures, (opts, start_time))
 	                acquire_data_prizm(fpga, opts, ndat=opts.nchan)
                 elif daq is 'singlesnap':
                         acquire_data_singlesnap(fpga, opts, ndat=opts.nchan)
