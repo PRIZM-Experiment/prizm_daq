e39d461e7e7cdb7d370ce187e9f8f4b2667011c3
nivekg <nivekghazi94@gmail.com>
2020-03-28 02:35:30 +0200

    * Removed supervisord stuff for the housekeeping pi since thats not   needed anymore
    * Removed supervisord stuff for executing the DAQ, in favour for
      parsing the appropriate supervisord config file as a runtime
      argument

--- supervisord/run_daq_supervised.py
+++ supervisord/run_daq_supervised.py
@@ -1,36 +1,24 @@
 #!/usr/bin/env /usr/bin/python
 
-import os, subprocess
+import os
+import subprocess
+import argparse
 
-# Before doing anything, check if supervisord is running already.  If
-# yes, then cry for help and don't do anything.
-txt = subprocess.Popen(['ps','-C','supervisord','-o','pid='], stdout=subprocess.PIPE).communicate()[0]
-txt = txt.strip()
-if len(txt) > 0:
-    print 'Looks like supervisord is already running under process ID',txt
-    print 'Quitting here and not starting DAQ.'
-    exit(0)
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser()
+	parser.add_argument("config_file", type=str, help="Path to supervisord configuration file")
+	args=parser.parse_args()
+	# Before doing anything, check if supervisord is running already.  If
+	# yes, then cry for help and don't do anything.
+	txt = subprocess.Popen(['ps','-C','supervisord','-o','pid='], stdout=subprocess.PIPE).communicate()[0]
+	txt = txt.strip()
+	if len(txt) > 0:
+    		print 'Looks like supervisord is already running under process ID',txt
+    		print 'Quitting here and not starting DAQ.'
+    		exit(0)
 
-# Check if we're on one of the SNAP Pis first
-for freq in ['70MHz', '100MHz', 'singlesnap']:
-    path = '/home/pi/data_'+freq
-    if os.path.exists(path):
-        print 'Found data path', path
-        print 'Running DAQ for', freq, 'MHz antenna'
-        cmd = 'supervisord -c /home/pi/daq_2019/supervisord/supervisord_'+freq+'.conf'
-        os.system(cmd)
-        print 'Started supervised DAQ process'
-        exit(0)
-
-# Check if we're on the switch control / housekeeping pi
-path = '/home/pi/switch_data'
-if os.path.exists(path):
-    print 'Found data path', path
-    print 'Running switch control and housekeeping script'
-    cmd = 'supervisord -c /home/pi/daq_2019/supervisord/supervisord_housekeeping.conf'
-    os.system(cmd)
-    print 'Started supervised DAQ process'
-    exit(0)
-
-# We shouldn't get to this point
-print 'Failed to autodetect data path, no DAQ/housekeeping started.  Halp!'
+	cmd = 'supervisord -c '+args.config_file
+        print 'Running cmd: '+cmd
+	os.system(cmd)
+	print 'Started supervised DAQ process'
+	exit(0)
