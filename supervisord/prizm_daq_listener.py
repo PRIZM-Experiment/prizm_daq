17618256a991f20ae9192424c3be77812fdfdcb3
nivekg <nivekghazi94@gmail.com>
2021-09-13 15:27:49 +0000

    Updated supervisord scripts

--- supervisord/prizm_daq_listener.py
+++ supervisord/prizm_daq_listener.py
@@ -13,7 +13,7 @@ def write_stderr(s):
 
 def main():
 
-    event_logfile = '/home/pi/supervisord_logs/found_events.txt'   # File for recorded crash events
+    event_logfile = 'found_events.txt'   # File for recorded crash events
     max_crash = 5    # Max number of crashes before reboot
     
     f=open(event_logfile,'w')
@@ -28,8 +28,11 @@ def main():
         if line.find('PROCESS_STATE_EXITED')>-1:
             ncrash=ncrash+1
             if ncrash==max_crash:
+                f.write('ncrash=' + repr(ncrash) + ' with event  ' + line)
                 f.write('Reached maximum number of allowed crashes, time to reboot\n')
                 f.write('Current time is '+datetime.datetime.utcnow().strftime('%Y%m%d_%H%M%S')+' \n')
+                f.flush()
+                f.close()
                 cmd = 'sudo reboot'  # Need to test this and see if we can do as not root
                 os.system(cmd)
 
--- supervisord/run_daq_supervised.py
+++ supervisord/run_daq_supervised.py
@@ -3,22 +3,41 @@
 import os
 import subprocess
 import argparse
+import datetime
+import logging
 
 if __name__ == "__main__":
-	parser = argparse.ArgumentParser()
-	parser.add_argument("config_file", type=str, help="Path to supervisord configuration file")
-	args=parser.parse_args()
-	# Before doing anything, check if supervisord is running already.  If
-	# yes, then cry for help and don't do anything.
-	txt = subprocess.Popen(['ps','-C','supervisord','-o','pid='], stdout=subprocess.PIPE).communicate()[0]
-	txt = txt.strip()
-	if len(txt) > 0:
-    		print 'Looks like supervisord is already running under process ID',txt
-    		print 'Quitting here and not starting DAQ.'
-    		exit(0)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("config_file", type=str, help="Path to supervisord configuration file")
+    parser.add_argument("log_dir", type=str, help = "Directory to output log folders")
+    args=parser.parse_args()
 
-	cmd = 'supervisord -c '+args.config_file
-        print 'Running cmd: '+cmd
-	os.system(cmd)
-	print 'Started supervised DAQ process'
-	exit(0)
+    t_start = str(datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
+    if not os.path.exists(args.log_dir+"/supervisord_logs/"+t_start):
+        print(args.log_dir+"/supervisord_logs/"+t_start)
+        os.makedirs(args.log_dir+"/supervisord_logs/"+t_start)
+
+    logger = logging.getLogger()
+    logger.setLevel(20)
+    f_handler = logging.FileHandler(args.log_dir+"/supervisord_logs/"+t_start+"/run_daq_supervised.log")
+    f_format = logging.Formatter('%(asctime)s %(name)-10s %(levelname)-8s %(message)s')
+    f_handler.setFormatter(f_format)
+    f_handler.setLevel(20)
+    logger.addHandler(f_handler)
+
+    logging.info("Starting run_daq_supervised.py script")
+
+    logging.info("Checking if supervisord is running already.")
+    txt = subprocess.Popen(['ps','-C','supervisord','-o','pid='], stdout=subprocess.PIPE).communicate()[0]
+    txt = txt.strip()
+    if len(txt) > 0:
+        logging.critical('Looks like supervisord is already running under process ID: '+str(txt))
+        logging.critical('Quitting here and not starting DAQ.')
+        exit(0)
+    else:
+        logging.info("Looks like no DAQ process is running, trying to start one now.")
+        cmd = 'supervisord -c '+args.config_file+' -d '+args.log_dir+"/supervisord_logs/"+t_start+' -l '+args.log_dir+"/supervisord_logs/"+t_start+"/supervisord.log -j "+args.log_dir+"/supervisord_logs/"+t_start+"/supervisord.pid"
+        logging.info('Running cmd: '+cmd)
+        os.system(cmd)
+        logging.info('Started supervised DAQ process')
+        exit(0)
